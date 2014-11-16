190a0ad11ebb998c8e1a840988c7e11d44367aea
ArvinGeek <fkwy0516@163.com>
2014-11-16 17:05:46 +0800

    Add
    add  add

--- CTest/CTest/main.c
+++ CTest/CTest/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int add(int a, int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+
+
 int main()
 {
     printf("Hello world!\n");
     printf("Hello My First GitHub Project About C!\n");
+    int a;
+    a=add(4,3);
+    printf("%d\n",a);
     return 0;
 }
